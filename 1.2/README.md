fc7560ef9c25f2a3d9f93cf29028d0e16b50322c
Michael <hulen20@gmail.com>
2021-06-29 15:20:37 +0800

    1

---
+++ 1.2/README.md
@@ -0,0 +1,11 @@
+安装
+
+https://www.rust-lang.org/learn/get-started
+
+插件
+Rust (rls) ：Adds language support for Rust to Visual Studio Code. Supports:
+
+CodeLLDB ：debug的一个插件
+
+crates ：方便包管理的插件
+
---
+++ 1.4/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mqiu/Documents/workspace/nodejs_workspace/rust_standard/1.4/hello_cargo/target/debug/hello_cargo: /Users/mqiu/Documents/workspace/nodejs_workspace/rust_standard/1.4/hello_cargo/src/main.rs
